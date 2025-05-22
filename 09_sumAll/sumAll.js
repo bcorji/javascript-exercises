ccca7bc5f9407129a0fe313d1904a7f6014dce44
Bliss Orji <chuksoriss@gmail.com>
2025-05-22 11:37:10 +0100

    Modify sumAll function to pass the tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,7 @@
 const sumAll = function(int1, int2) {
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+    if (int1 < 0 || int2 < 0) return "ERROR";
+
     let sum = 0;
     for (let i = int1; i <= int2, i++;){
         sum += i;
